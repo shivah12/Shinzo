9e6e7d9a9dff56e44d05c859c88fcaad2700ee84
Shreeti <114981873+shivah12@users.noreply.github.com>
2024-01-18 16:11:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import PomodoroTimer from "./pages/PomodoroTimer";
 import AnalogClock from "./pages/AnalogClock";
 import Todo from "./pages/Todo";
 import Footer from "./pages/Footer";
-import backgroundAudio from "./assets/audio.mp3"; // Replace with the correct path
+ // Replace with the correct path
 
 function VideoMessage() {
   return (
@@ -21,28 +21,11 @@ function VideoMessage() {
 }
 
 function App() {
-  const audioRef = useRef(null);
-
-  useEffect(() => {
-    // Play audio when the component mounts
-    if (audioRef.current) {
-      audioRef.current.play().catch(error => {
-        // Autoplay was prevented, handle the error
-        console.error('Autoplay prevented:', error);
-      });
-    }
-  }, []);
-
   return (
     <>
       <Router>
         <div className="App">
           <SideBar />
-          <audio ref={audioRef} loop>
-            <source src={backgroundAudio} type="audio/mp3" />
-            Your browser does not support the audio tag.
-          </audio>
-
           <div className="content">
             <Routes>
               <Route path="/" element={<><Home /><AnalogClock /></>} />
